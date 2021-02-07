cc3c01fc5860020bda66d5150d95724ea1dad327
jiweiqi <jiweiqi10@gmail.com>
2021-02-07 17:58:38 -0500

    add dependence

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
 using Arrhenius
 using Test
 
-@testset "Arrhenius.jl" begin
+@testset "jl" begin
     # Write your tests here.
     # TODO: test not implemented yet
 
-    gas = Arrhenius.CreateSolution("python/JP10skeletal.yaml")
-    mgas = Arrhenius.CreateMSolution(gas)
+    gas = CreateSolution("python/JP10skeletal.yaml")
+    mgas = CreateMSolution(gas)
 
-    Arrhenius.set_states(gas, mgas, 1200., Arrhenius.one_atm, ones(gas.n_species) ./ gas.n_species)
+    set_states(gas, mgas, 1200., one_atm, ones(gas.n_species) ./ gas.n_species)
 
     @show mgas.ρ_mass
     @show mgas.wdot
