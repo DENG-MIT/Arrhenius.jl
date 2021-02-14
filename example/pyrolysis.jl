b4852f13bcd15beb555911e6e3902b86429a5aba
jiweiqi <jiweiqi10@gmail.com>
2021-02-14 11:17:49 -0500

    update pyrolysis example

--- example/pyrolysis.jl
+++ example/pyrolysis.jl
@@ -60,10 +60,15 @@ println("timing dudt ...")
 @time dudt(u0)
 @time du0 = ForwardDiff.gradient(dudt, u0)
 @time du0 = ForwardDiff.gradient(dudt, u0)
+# timing dudt ...
+# 1.949944 seconds (5.23 M allocations: 262.966 MiB, 3.79% gc time)
+# 0.000183 seconds (48 allocations: 22.797 KiB)
+# 4.014816 seconds (9.36 M allocations: 458.699 MiB, 2.71% gc time)
+# 0.001779 seconds (407 allocations: 981.609 KiB)
 
 tspan = [0.0, 0.07];
 prob = ODEProblem(dudt!, u0, tspan);
-sol = solve(prob, KenCarp5(autodiff=false), reltol=1e-6, abstol=1e-9)
+sol = solve(prob, TRBDF2(), reltol=1e-6, abstol=1e-9)
 
 plt = plot(sol.t, sol[species_index(gas, "C10H16"), :], lw=2, label="Arrhenius.jl");
 plot!(plt, ct_ts, ct_Y[:, species_index(gas, "C10H16")], label="Cantera")
@@ -96,6 +101,11 @@ println("timing ode solver ...")
 @time fsol(u0)
 @time ForwardDiff.gradient(fsol, u0)
 @time ForwardDiff.gradient(fsol, u0)
+# timing ode solver ...
+# 0.405083 seconds (614.32 k allocations: 45.126 MiB)
+# 0.036229 seconds (16.72 k allocations: 11.618 MiB)
+# 41.850114 seconds (90.09 M allocations: 4.771 GiB, 3.85% gc time)
+# 1.517267 seconds (183.25 k allocations: 864.085 MiB, 7.46% gc time)
 
 # Profile.clear
 # @profile ForwardDiff.gradient(x -> fsol(x), u0)
