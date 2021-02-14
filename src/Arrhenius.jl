55f2fe58c0846910adc74dae702964ca1ee5a308
jiweiqi <jiweiqi10@gmail.com>
2021-02-13 22:59:57 -0500

    update test

--- src/Arrhenius.jl
+++ src/Arrhenius.jl
@@ -2,6 +2,7 @@ module Arrhenius
     using YAML
     using NPZ
     using LinearAlgebra
+
     include("Constants.jl")
     include("DataStructure.jl")
     include("Solution.jl")
@@ -34,16 +35,34 @@ end
 #     end
 # end
 # @time profile_test(1000)
-# Profile.clear
-# @profile profile_test(1000)
-# Juno.profiler(; C = false)
+#
+# # Profile.clear
+# # @profile profile_test(1000)
+# # Juno.profiler(; C = false)
 #
 # u0 = vcat(Y0, T0);
+# R = Arrhenius.R
 # function f(u)
-#     Arrhenius.set_states(gas, u[end], P, u[1:ns])[10]
+#     # Arrhenius.set_states(gas, u[end], P, u[1:ns])[10]
+#     T = u[end]
+#     Y = @view(u[1:ns])
+#     mean_MW = 1. / dot(Y, 1 ./ gas.MW)
+#     ρ_mass = P / R / T * mean_MW
+#     X =  Arrhenius.Y2X(gas, Y, mean_MW)
+#     C =  Arrhenius.Y2C(gas, Y, ρ_mass)
+#     cp_mole, cp_mass =  Arrhenius.get_cp(gas, T, X, mean_MW)
+#     h_mole =  Arrhenius.get_H(gas, T, Y, X)
+#     S0 =  Arrhenius.get_S(gas, T, P, X)
+#     wdot = Arrhenius.wdot_func(gas.reaction, T, C, S0, h_mole)
+#     return wdot[end]
 # end
-# @time grad = ForwardDiff.gradient(x -> f(x), u0)
+# u0[end] = 1200 + rand()
+# @time f(u0)
+# @time f(u0)
 #
+# @time grad = ForwardDiff.gradient(f, u0)
+# @time grad = ForwardDiff.gradient(f, u0)
+
 # Profile.clear
 # @profile grad = ForwardDiff.gradient(x -> f(x), u0)
 # Juno.profiler(; C = false)
