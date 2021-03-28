74cd4bf0c2d43a27ea85bc5280559ba91f141913
jiweiqi <jiweiqi10@gmail.com>
2021-03-28 15:01:44 -0400

    minor

--- src/Arrhenius.jl
+++ src/Arrhenius.jl
@@ -14,51 +14,51 @@ end
 ```
 Following codes are used during development phase only.
 ```
-using LinearAlgebra
-using DifferentialEquations
-using Sundials
-using Profile
-using ForwardDiff
-using SparseArrays
-
-R = Arrhenius.R
-one_atm = Arrhenius.one_atm
-
-# gas = Arrhenius.CreateSolution("./mechanism/nc7_ver3.1_mech.yaml")
+# using LinearAlgebra
+# using DifferentialEquations
+# using Sundials
+# using Profile
+# using ForwardDiff
+# using SparseArrays
+#
+# R = Arrhenius.R
+# one_atm = Arrhenius.one_atm
+#
+# # gas = Arrhenius.CreateSolution("./mechanism/nc7_ver3.1_mech.yaml")
+# # const ns = gas.n_species
+# # @show gas.thermo.nasa_high[Arrhenius.species_index(gas, "O2"), :]
+# # Y0 = zeros(ns)
+# # Y0[Arrhenius.species_index(gas, "NC7H16")] = 0.1
+# # Y0[Arrhenius.species_index(gas, "O2")] = 0.2
+# # Y0[Arrhenius.species_index(gas, "N2")] = 0.7
+#
+# gas = Arrhenius.CreateSolution("./mechanism/gri30.yaml")
 # const ns = gas.n_species
 # @show gas.thermo.nasa_high[Arrhenius.species_index(gas, "O2"), :]
 # Y0 = zeros(ns)
-# Y0[Arrhenius.species_index(gas, "NC7H16")] = 0.1
+# Y0[Arrhenius.species_index(gas, "CH4")] = 0.1
 # Y0[Arrhenius.species_index(gas, "O2")] = 0.2
 # Y0[Arrhenius.species_index(gas, "N2")] = 0.7
-
-gas = Arrhenius.CreateSolution("./mechanism/gri30.yaml")
-const ns = gas.n_species
-@show gas.thermo.nasa_high[Arrhenius.species_index(gas, "O2"), :]
-Y0 = zeros(ns)
-Y0[Arrhenius.species_index(gas, "CH4")] = 0.1
-Y0[Arrhenius.species_index(gas, "O2")] = 0.2
-Y0[Arrhenius.species_index(gas, "N2")] = 0.7
-
-T0 = 1200.0
-P = Arrhenius.one_atm
-wdot = Arrhenius.set_states(gas, T0, P, Y0)
-@show wdot
-
-T = T0
-Y = Y0
-mean_MW = 1. / dot(Y, 1 ./ gas.MW)
-ρ_mass = P / R / T * mean_MW
-X =  Arrhenius.Y2X(gas, Y, mean_MW)
-C =  Arrhenius.Y2C(gas, Y, ρ_mass)
-cp_mole, cp_mass =  Arrhenius.get_cp(gas, T, X, mean_MW)
-cv_mole, cv_mass = Arrhenius.get_cv(cp_mole, cp_mass, mean_MW)
-
-h_mole =  Arrhenius.get_H(gas, T, Y, X)
-u_mole = Arrhenius.get_U(h_mole, Y)
-
-S0 =  Arrhenius.get_S(gas, T, P, X)
-wdot = Arrhenius.wdot_func(gas.reaction, T, C, S0, h_mole; get_qdot=false)
+#
+# T0 = 1200.0
+# P = Arrhenius.one_atm
+# wdot = Arrhenius.set_states(gas, T0, P, Y0)
+# @show wdot
+#
+# T = T0
+# Y = Y0
+# mean_MW = 1. / dot(Y, 1 ./ gas.MW)
+# ρ_mass = P / R / T * mean_MW
+# X =  Arrhenius.Y2X(gas, Y, mean_MW)
+# C =  Arrhenius.Y2C(gas, Y, ρ_mass)
+# cp_mole, cp_mass =  Arrhenius.get_cp(gas, T, X, mean_MW)
+# cv_mole, cv_mass = Arrhenius.get_cv(cp_mole, cp_mass, mean_MW)
+#
+# h_mole =  Arrhenius.get_H(gas, T, Y, X)
+# u_mole = Arrhenius.get_U(h_mole, Y)
+#
+# S0 =  Arrhenius.get_S(gas, T, P, X)
+# wdot = Arrhenius.wdot_func(gas.reaction, T, C, S0, h_mole; get_qdot=false)
 
 
 # u0 = vcat(Y0, T0);
